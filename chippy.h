7445513eb3b814171d4fd2f6c725073f3ff4d6c0
Sean Siddens <seansiddens@gmail.com>
2021-03-25 18:38:50 -0700

    Added function declarations

---
+++ chippy.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Function declarations;
+void update_timers();
+void load_program(char *file_name);
+void load_font();
+void step();
+void update_screen(SDL_Renderer *window_renderer);
+void clear_screen();
